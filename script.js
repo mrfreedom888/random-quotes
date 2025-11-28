fd2ee5d799d505a129ea8bb162015a05ab7bbe55
mrfreedom888 <example@gmail.com>
2025-11-28 11:23:45 +0200

    Added cards and remove quotes

--- script.js
+++ script.js
@@ -2,13 +2,50 @@ import quotes from "./quotes.js";
 
 const quoteElement = document.getElementById("quote");
 const generateBnt = document.getElementById("generate-btn");
+const quoteAuthorElement = document.getElementById("quote-author");
+const toggleFavoritreBtn = document.getElementById("toggle-favorite-btn");
+const favoriteContainer = document.getElementById("favorite-container");
+
+let currentQuoteIndex;
 
 function geterateRandomQuote() {
-  // console.log("THIS FUNCTIOTN WAS JUST CALLED");
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  const randomQuote = quotes[randomIndex];
-  const quote = `<em>'${randomQuote.quote}'</em><br>${randomQuote.author}`;
-  quoteElement.innerHTML = quote;
+  currentQuoteIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[currentQuoteIndex];
+  const { quote, author } = randomQuote;
+  quoteElement.textContent = quote;
+  quoteAuthorElement.textContent = author;
+  toggleFavoritreBtn.textContent = randomQuote.isFavorite
+    ? "Remove from favorites"
+    : "Add to favorites";
+
+  toggleFavoritreBtn.style.display = "inline-block";
+}
+
+function toggleFavorite() {
+  const currentQuote = quotes[currentQuoteIndex];
+  currentQuote.isFavorite = !currentQuote.isFavorite;
+  toggleFavoritreBtn.textContent = currentQuote.isFavorite
+    ? "Remove from favorites"
+    : "Add to favorites";
+  if (currentQuote.isFavorite) {
+    const favoriteCard = document.createElement("div");
+    favoriteCard.classList.add("favorite-card");
+    favoriteCard.innerHTML = `
+      <p>${currentQuote.quote}</p>
+      <p class = 'author'>${currentQuote.author}</p>
+      `;
+    favoriteContainer.appendChild(favoriteCard);
+  } else {
+    const favoriteCards = document.querySelectorAll(".favorite-card");
+    favoriteCards.forEach((card) => {
+      if (card.textContent.includes(currentQuote.quote)) {
+        card.remove();
+      }
+    });
+  }
 }
 
 generateBnt.addEventListener("click", geterateRandomQuote);
+toggleFavoritreBtn.addEventListener("click", toggleFavorite);
+
+// geterateRandomQuote();
